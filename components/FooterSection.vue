081d1c5e168dfd7f663599317a84c322a0ce8e2f
tea-egg-tw <bm6hjj2020.tw@gmail.com>
2026-03-11 00:26:49 +0800

    chore: 加入 GitHub Actions 自動部署腳本

---
+++ components/FooterSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="custom-footer">
+    <div class="copyright">&copy; {{ new Date().getFullYear() }} 霜田. All Rights Reserved.</div>
+  </footer>
+</template>
+
+<style scoped>
+.custom-footer {
+  background-color: #EFECE5;
+  padding: 3rem;
+  text-align: center;
+  border-top: 1px solid rgba(184, 166, 150, 0.3);
+  color: #B8A696;
+}
+</style>
